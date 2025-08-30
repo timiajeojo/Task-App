92c1b264759436983fc7de2ecb9cb50cf6828c21
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-30 10:54:54 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const taskInput = document.getElementById("task-input");
+const inputBtn = document.getElementById("input-btn");
+const taskList = document.getElementById("task-list");
+const heading = document.getElementById("task-heading");
+const completedList = document.getElementById("completed-list");
+const toggleBtn = document.getElementById("toggle-completed");
