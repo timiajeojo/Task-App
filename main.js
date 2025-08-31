ce7f418ae9d39608c66846614498606b99642a19
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-31 18:04:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const taskList = document.getElementById("task-list");
 const heading = document.getElementById("task-heading");
 const completedList = document.getElementById("completed-list");
 const toggleBtn = document.getElementById("toggle-completed");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() {
+  taskList.innerHTML = "";
+  completedList.innerHTML = "";
+
+  let completedCount = 0;
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task.text + " "
+    if (task.completed) {
+      li.classList.add("completed")
+    }
+    
